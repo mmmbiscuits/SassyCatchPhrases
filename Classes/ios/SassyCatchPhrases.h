3f77bd8ddcf9cad36decaba54cdca5850d6b40e2
Ryan Smale <eagle14@gmail.com>
2014-04-03 15:14:18 +1300

    first try podspec

---
+++ Classes/ios/SassyCatchPhrases.h
@@ -0,0 +1,14 @@
+//
+//  SassyCatchPhrases.h
+//  SassyCatchPhrases
+//
+//  Created by Ryan Smale on 3/04/14.
+//  Copyright (c) 2014 Ryan Smale. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SassyCatchPhrases : NSObject
+
++(NSString*)hitMeWithAPhrase;
+@end
